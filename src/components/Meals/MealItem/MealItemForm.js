9325638661d5c8a88ebb71f4f300c9d47bc05d49
Marko Blazevic <www.markoblazevic@gmail.com>
2021-09-07 13:34:59 -0300

    Added Ref to Amount and connection with HeaderCart

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import classes from './MealItemForm.module.css';
 import Input from '../../UI/Input';
 
 const MealItemForm = (props) => {
+  const inputRef = useRef();
+  const [inputAmountError, setInputAmountError] = useState(false);
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    const inputRefAmount = inputRef.current.value;
+    const inputRefAmountNumber = +inputRefAmount;
+    if (inputRefAmount.trim().length === 0) {
+      setInputAmountError(true);
+      return;
+    }
+    setInputAmountError(false);
+    props.addItem(inputRefAmountNumber);
+  };
+
   return (
-    <div className={classes.form}>
+    <form className={classes.form} onSubmit={onSubmitHandler}>
       <Input
+        ref={inputRef}
         label='Amount'
         input={{
           id: 'amount_' + props.id,
@@ -15,7 +31,13 @@ const MealItemForm = (props) => {
         }}
       />
       <button>+ Add</button>
-    </div>
+
+      {inputAmountError && (
+        <p className={classes.error}>
+          Please correct the Amount of your meals!
+        </p>
+      )}
+    </form>
   );
 };
 
--- src/store/CartContexProvider.js
+++ src/store/CartContexProvider.js
@@ -1,12 +1,31 @@
+import { useReducer } from 'react';
 import CartContext from './cart-context';
 
+const defaultState = { items: [], totalAmount: 0 };
+
 const CartContexProvider = (props) => {
-  const addItemToCart = (item) => {};
-  const removeItemFromCart = (id) => {};
+  const cartReducer = (cartState, action) => {
+    if (action.type === 'ADD') {
+      const updatedCartItems = cartState.items.concat(action.item);
+      const updatedTotalAmount =
+        cartState.totalAmount + action.item.price * action.item.amount;
+      return { items: updatedCartItems, totalAmount: updatedTotalAmount };
+    }
+    return defaultState;
+  };
+
+  const [cartState, cartDispatchFn] = useReducer(cartReducer, defaultState);
+
+  const addItemToCart = (item) => {
+    cartDispatchFn({ type: 'ADD', item: item });
+  };
+  const removeItemFromCart = (id) => {
+    cartDispatchFn({ type: 'REMOVE', id: id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCart,
     removeItem: removeItemFromCart,
   };
