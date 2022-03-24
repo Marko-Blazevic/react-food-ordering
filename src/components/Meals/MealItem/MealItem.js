babb6ba6414c0991a80e395c51cf62ff77be2af1
Marko Blazevic <www.markoblazevic@gmail.com>
2022-03-24 10:18:19 -0300

    Added $ sign to price

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -21,7 +21,7 @@ const MealItem = (props) => {
       <div>
         <h3>{props.name}</h3>
         <div className={classes.description}>{props.description}</div>
-        <div className={classes.price}>{price}</div>
+        <div className={classes.price}>${price}</div>
       </div>
       <div>
         <MealItemForm id={props.id} addItem={addItemHandler} />
